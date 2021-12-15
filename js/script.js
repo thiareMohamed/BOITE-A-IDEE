1ef05fe8463c125351d4cebe6cbb4f84e73da5fc
thiareMohamed <thiaremohamed.mt@gmail.com>
2021-12-15 12:30:18 +0000

    initialisation des fichiers

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var donees =
+[
+    {
+        id : 1, titre : 'Cours', idee : 'lorem impus ldie dsdnsu sdksu efibv dsdid udsd ', statu : 'true'
+    },
+    {
+        id : 2, titre : 'Cours', idee : 'lorem impus ldie dsdnsu sdksu efibv dsdid udsd ', statu : 'true'
+    },
+    {
+        id : 3, titre : 'Cours', idee : 'lorem impus ldie dsdnsu sdksu efibv dsdid udsd ', statu : 'true'
+    },
+]
