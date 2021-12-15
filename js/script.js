3f44decf95c061f242db27824097607464ad3e41
thiareMohamed <thiaremohamed.mt@gmail.com>
2021-12-15 13:43:44 +0000

    feat index.html statique

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var donees =
+var donnees =
 [
     {
         id : 1, titre : 'Cours', idee : 'lorem impus ldie dsdnsu sdksu efibv dsdid udsd ', statu : 'true'
@@ -9,4 +9,65 @@ var donees =
     {
         id : 3, titre : 'Cours', idee : 'lorem impus ldie dsdnsu sdksu efibv dsdid udsd ', statu : 'true'
     },
+    {
+        id : 3, titre : 'Cours', idee : 'lorem impus ldie dsdnsu sdksu efibv dsdid udsd ', statu : 'true'
+    },
+    {
+        id : 3, titre : 'Cours', idee : 'lorem impus ldie dsdnsu sdksu efibv dsdid udsd ', statu : 'true'
+    },
+    {
+        id : 3, titre : 'Cours', idee : 'lorem impus ldie dsdnsu sdksu efibv dsdid udsd ', statu : 'true'
+    },
+    {
+        id : 3, titre : 'Cours', idee : 'lorem impus ldie dsdnsu sdksu efibv dsdid udsd ', statu : 'true'
+    }, {
+        id : 3, titre : 'Cours', idee : 'lorem impus ldie dsdnsu sdksu efibv dsdid udsd ', statu : 'true'
+    }, {
+        id : 3, titre : 'Cours', idee : 'lorem impus ldie dsdnsu sdksu efibv dsdid udsd ', statu : 'true'
+    }, {
+        id : 3, titre : 'Cours', idee : 'lorem impus ldie dsdnsu sdksu efibv dsdid udsd ', statu : 'true'
+    }, {
+        id : 3, titre : 'Cours', idee : 'lorem impus ldie dsdnsu sdksu efibv dsdid udsd ', statu : 'true'
+    }, {
+        id : 3, titre : 'Cours', idee : 'lorem impus ldie dsdnsu sdksu efibv dsdid udsd ', statu : 'true'
+    }, {
+        id : 3, titre : 'Cours', idee : 'lorem impus ldie dsdnsu sdksu efibv dsdid udsd ', statu : 'true'
+    },
 ]
+
+for (let donnee of donnees)
+{
+    afficheIdee (donnee.titre,donnee.idee)
+}
+
+
+function afficheIdee (titre,idee)
+{
+
+    var main = document.querySelector('.main-idee')
+    var card = document.createElement("div");
+    main.appendChild(card);
+    card.classList.add('card')
+    card.classList.add('border-primary')
+    card.classList.add('m-auto')
+    card.classList.add('mb-3')
+    card.style.width = "18rem";
+     
+    var header = document.createElement("h5");
+    card.appendChild(header);
+    header.classList.add('card-header')
+    header.classList.add('text-primary')
+    header.classList.add('border-primary')
+    header.innerHTML = titre
+
+    var body = document.createElement("div");
+    card.appendChild(body);
+    body.classList.add('card-body')
+    body.innerHTML = idee
+
+    var footer = document.createElement("div");
+    card.appendChild(footer);
+    footer.classList.add('card-footer')
+    footer.classList.add('border-primary')
+
+}
